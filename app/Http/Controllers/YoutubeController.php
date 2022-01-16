f2f782508a23516beaea99f693b6ec69a6da9b33
tanjed <tanjed@agorik.tech>
2022-01-16 19:26:16 +0600

    Seperator added

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -86,7 +86,7 @@ class YoutubeController extends Controller
     }
 
     public function clean($string) {
-        $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+        $string = str_replace(' ', '_', $string); // Replaces all spaces with hyphens.
 
         return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
     }
--- app/Services/Downloader.php
+++ app/Services/Downloader.php
@@ -38,7 +38,7 @@ class Downloader
     }
 
     public static function clean($string) {
-        $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+        $string = str_replace(' ', '_', $string); // Replaces all spaces with hyphens.
 
         return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
     }
