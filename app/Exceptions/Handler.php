8a5a6c058e978901a93c79fd540fffa3c9cffb5a
tanjed <tanjed@agorik.tech>
2022-01-17 17:49:52 +0600

    Route not found redirection

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +52,8 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof RouteNotFoundException || $exception instanceof MethodNotAllowedException)
+            return redirect('/');
         return parent::render($request, $exception);
     }
 }
